6d17958a1a9c33a8c2b2ceed0b5e66e6bd4a6eed
Damian Pantano <damian@needle.com>
2015-07-09 12:29:02 -0600

    GREEB is not a color, also cleaned up weird color choices

--- lib/coloured_messages.sh
+++ lib/coloured_messages.sh
@@ -1,59 +1,48 @@
 #!/bin/bash
 message(){
 
-	MESSAGE=$1
-	LEVEL=$2
-	START='\033['
-	FINISH='\033[0m'
+  MESSAGE=$1
+  LEVEL=$2
+  START='\033['
+  FINISH='\033[0m'
 
-	BOLD='1;'
+  BOLD='1;'
 
-	BLACK="30"
-	RED="31"
-	GREEB="32"
-	YELLOW="33"
-	BLUE="34"
-	MAGENTA="35"
-	CYAN="36"
-	GREY="37"
+  RED="31"
+  GREEN="32"
+  YELLOW="33"
+  BLUE="34"
+  MAGENTA="35"
+  CYAN="36"
+  GREY="37"
 
-	BGBLACK="40"
-	BGRED="41"
-	BGGREEB="42"
-	BGYELLOW="43"
-	BGBLUE="44"
-	BGMAGENTA="45"
-	BGCYAN="46"
-	BGGREY="47"
+  case $LEVEL in
+    "header" )
+      COLOURS="$BOLD;${GREY}m"
+      ;;
+    "error" )
+      COLOURS="${RED}m"
+      ;;
+    "warning" )
+      COLOURS="${MAGENTA}m"
+      ;;
+    "debug" )
+      COLOURS="${YELLOW}m"
+      ;;
+    "result" )
+      COLOURS="${CYAN}m"
+      ;;
+    "success" )
+      COLOURS="${GREEN}m"
+      ;;
+    "choice" )
+      COLOURS="${BLUE}m"
+      ;;
+    * )
+      COLOURS="${GREY}m"
+      ;;
+  esac
 
-	case $LEVEL in
-		"header" )
-			COLOURS="$BOLD;$GREY;${BGBLUE}m"
-			;;
-		"error" )
-			COLOURS="$GREY;${BGRED}m"
-			;;
-		"warning" )
-			COLOURS="$BLACK;${BGYELLOW}m"
-			;;
-		"debug" )
-			COLOURS="${YELLOW}m"
-			;;
-		"result" )
-			COLOURS="${CYAN}m"
-			;;
-		"success" )
-			COLOURS="${GREEN}m"
-			;;
-		"choice" )
-			COLOURS="${BLUE}m"
-			;;
-		* )
-			COLOURS="${GREY}m"
-			;;
-	esac
-
-	echo -e "${START}${COLOURS} ${MESSAGE} ${FINISH}"
-	tput sgr0
+  echo -e "${START}${COLOURS} ${MESSAGE} ${FINISH}"
+  tput sgr0
 }
-
